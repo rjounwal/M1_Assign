a41623fb6abe3a974eeb4737c750a6e31d6dc27b
Rajesh <rajesh.jounwal@aricent.com>
2018-08-31 00:15:54 +0530

    Pushing TestClass.java file

---
+++ sample-project/src/TestClass.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.utils.*;
+
+public class TestClass{
+
+	public static void main(String args[]){
+		
+		harryCode();
+		kaneCode();
+		
+	}
+
+	private static void harryCode(){
+		System.out.println("This is Harry Changes");
+	}
+
+	private static void kaneCode(){
+		System.out.println("This is Kane Changes");
+	}
+}
